be1ad7e93466e78d731548be602cd68114e10229
ilkutkutlar <ilkutkutlar@gmail.com>
2018-12-29 17:23:24 +0000

    GUI: Add reaction dialog is complete

--- code/gui2.py
+++ code/gui2.py
@@ -16,77 +16,89 @@ class AddReactionDialog(QDialog):
         self.combo.addItem("mRNA Degradation Reaction")
         self.combo.addItem("Protein Degradation Reaction")
         self.combo.addItem("Custom Reaction")
+        self.combo.currentIndexChanged.connect(self._handler_reaction_type_changed)
 
-        self.combo.currentIndexChanged.connect(self._reaction_type_changed)
-        self.combo.setCurrentIndex(0)
-
-    def _init_fields(self):
+    def _init_form_fields(self):
         self.fields = {}
 
         # TODO: Regulation!
-
         # Common
         self.fields["name"] = QLineEdit()
+        self.fields["name"].setPlaceholderText("Name")
 
         # Transcription
         self.fields["transcription_rate"] = QLineEdit()
+        self.fields["transcription_rate"].setPlaceholderText("Transcription Rate")
+
         self.fields["kd"] = QLineEdit()
+        self.fields["kd"].setPlaceholderText("Kd")
+
         self.fields["hill_coefficient"] = QLineEdit()
+        self.fields["hill_coefficient"].setPlaceholderText("Hill coefficient")
 
         # Translation
         self.fields["translation_rate"] = QLineEdit()
+        self.fields["translation_rate"].setPlaceholderText("Tranlation Rate")
 
         # mRNA decay
         self.fields["mrna_decay_rate"] = QLineEdit()
+        self.fields["mrna_decay_rate"].setPlaceholderText("Decay Rate")
 
         # Protein decay
         self.fields["protein_decay_rate"] = QLineEdit()
+        self.fields["protein_decay_rate"].setPlaceholderText("Decay Rate")
 
         # Custom reaction
         self.fields["custom_equation"] = QLineEdit()
+        self.fields["custom_equation"].setPlaceholderText("Equation")
 
-    def _clear_form_fields(self):
         for field in self.fields:
-            self.fields[field].setParent(None)
+            self.fields[field].setVisible(False)
+            self.form.addWidget(self.fields[field])
 
+        # Make the initial fields visible (i.e. the combo box has this reaction
+        # selected when the dialog is first opened)
+        self.fields["name"].setVisible(True)
+        self.fields["transcription_rate"].setVisible(True)
+        self.fields["kd"].setVisible(True)
+        self.fields["hill_coefficient"].setVisible(True)
 
-        # for field in self.fields:
-        #     self.form.removeRow(self.fields[field])
+    def _handler_reaction_type_changed(self, index):
+        for field in self.fields:
+            self.fields[field].setVisible(False)
 
-    def _reaction_type_changed(self, index):
-        self._clear_form_fields()
-        self.form.addRow("Name: ", self.fields["name"])
+        self.fields["name"].setVisible(True)
 
         if index == 0:
-            self.form.addRow("Transcription Rate: ", self.fields["transcription_rate"])
-            self.form.addRow("Kd: ", self.fields["kd"])
-            self.form.addRow("Hill coefficient: ", self.fields["hill_coeff"])
+            self.fields["transcription_rate"].setVisible(True)
+            self.fields["kd"].setVisible(True)
+            self.fields["hill_coefficient"].setVisible(True)
         elif index == 1:
-            self.form.addRow("Tranlation Rate: ", self.fields["translation_rate"])
+            self.fields["translation_rate"].setVisible(True)
         elif index == 2:
-            self.form.addRow("Decay Rate: ", self.fields["mrna_decay_rate"])
+            self.fields["mrna_decay_rate"].setVisible(True)
         elif index == 3:
-            self.form.addRow("Decay Rate: ", self.fields["protein_decay_rate"])
+            self.fields["protein_decay_rate"].setVisible(True)
         elif index == 4:
-            self.form.addRow("Equation: ", self.fields["custom_equation"])
-        self.form.update()
+            self.fields["custom_equation"].setVisible(True)
 
     def _init_ui(self):
-        self.layout = QVBoxLayout()
-        self.form = QFormLayout()
-        self.combo = QComboBox()
+        self.layout = QVBoxLayout()  # The main layout
+        self.form = QVBoxLayout()  # This holds the text fields for reaction parameters
+        self.combo = QComboBox()  # Choose the type of reaction
         self.ok_button = QPushButton("OK", self)
 
         self._init_combo()
-        self._init_fields()
+        self._init_form_fields()
 
         self.layout.addWidget(self.combo)
         self.layout.addLayout(self.form)
         self.layout.addWidget(self.ok_button)
+
         self.setLayout(self.layout)
 
         self.setWindowTitle("Add reaction")
-        self.setWindowModality(Qt.ApplicationModal)
+        self.setWindowModality(Qt.WindowModal)
         self.exec_()
 
 

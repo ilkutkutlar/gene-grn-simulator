17cbed515a97b7e42a08cdd1e426d36f6e69d700
ilkutkutlar <ilkutkutlar@gmail.com>
2019-01-26 20:10:43 +0000

    BROKEN: Fixed some bugs with the ODE simulation

--- code/ode_simulator.py
+++ code/ode_simulator.py
@@ -1,9 +1,11 @@
+from math import log, e
 from typing import List, Dict, NamedTuple
 
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
 
+from formulae import TranscriptionFormula, DegradationFormula, TranslationFormula
 from gillespie_simulator import SimulationResults
 from models.network import Network
 from models.reaction import Reaction
@@ -62,15 +64,15 @@ class OdeSimulator:
         t: list = np.linspace(sim.start_time, sim.end_time, 1000)
 
         # solve the ODEs
-        solution = odeint(OdeSimulator.dy_dt, y0, t, [net])
+        solution = odeint(OdeSimulator.dy_dt, y0, t, args=(net,))
 
         results: SimulationResults = []
         for x in solution:
-            specs = Dict[str, float]
+            specs: Dict[str, float] = {}
 
             for y in x:
-                for s in net.species:
-                    specs[s] = y
+                for sp in net.species:
+                    specs[sp] = y
 
             results.append((x, specs))
 
@@ -158,3 +160,80 @@ class OdeSimulator:
         return rps
 
     # endregion
+
+
+if __name__ == '__main__':
+    # p_lacI0 = 10
+    # p_tetR0 = 10
+    # p_cl0 = 10
+
+    # m_lacI0 = 100
+    # m_tetR0 = 80
+    # m_cl0 = 50
+
+    # region Constants
+
+    ps_active = 0.5  # Promoter strength (active)
+    ps_repr = 5 * (10 ** -4)  # Promoter strength (repressed)
+    transcription_rate_active = ps_active * 60
+    transcription_rate_repr = ps_repr * 60
+
+    # Decay-related values
+    mRNA_half_life = 2
+    protein_half_life = 10
+    mRNA_decay_rate = log(2, e) / mRNA_half_life
+    protein_decay_rate = log(2, e) / protein_half_life
+
+    # Translation-related values
+    translation_efficiency = 19.97
+    translation_rate = translation_efficiency * mRNA_decay_rate
+
+    # Other values
+    hill_coeff = 2  # Hill coefficient
+    Km = 40  # Activation coefficient
+
+    # Derived values
+
+    # Active transcription rate (rescaled?)
+    alpha = transcription_rate_active * translation_efficiency \
+            * protein_half_life / (log(2, e) * Km)
+    # Repressed transcription rate (rescaled?)
+    alpha0 = transcription_rate_repr * translation_efficiency \
+             * protein_half_life / (log(2, e) * Km)
+    # Translation rate
+    beta = protein_decay_rate / mRNA_decay_rate
+
+    # endregion
+
+    species = {"laci_mrna": 0, "tetr_mrna": 20, "cl_mrna": 0,
+               "laci_p": 0, "tetr_p": 0, "cl_p": 0}
+
+    regulations = [Regulation(from_gene="cl_p", to_gene="laci_mrna", reg_type=RegType.REPRESSION),
+                   Regulation(from_gene="laci_p", to_gene="tetr_mrna", reg_type=RegType.REPRESSION),
+                   Regulation(from_gene="tetr_p", to_gene="cl_mrna", reg_type=RegType.REPRESSION)]
+
+    reactions = [Reaction([], ["laci_mrna"], TranscriptionFormula(alpha, 2, 40, "laci_mrna")),
+                 Reaction([], ["tetr_mrna"], TranscriptionFormula(alpha, 2, 40, "tetr_mrna")),
+                 Reaction([], ["cl_mrna"], TranscriptionFormula(alpha, 2, 40, "cl_mrna")),
+
+                 Reaction(["laci_mrna"], [""], DegradationFormula(mRNA_decay_rate, "laci_mrna")),
+                 Reaction(["tetr_mrna"], [""], DegradationFormula(mRNA_decay_rate, "tetr_mrna")),
+                 Reaction(["cl_mrna"], [""], DegradationFormula(mRNA_decay_rate, "cl_mrna")),
+
+                 Reaction(["laci_mrna"], ["laci_p"], TranslationFormula(beta, "laci_mrna")),
+                 Reaction(["tetr_mrna"], ["tetr_p"], TranslationFormula(beta, "tetr_mrna")),
+                 Reaction(["cl_mrna"], ["cl_p"], TranslationFormula(beta, "cl_mrna")),
+
+                 Reaction(["laci_p"], [""], DegradationFormula(protein_decay_rate, "laci_p")),
+                 Reaction(["tetr_p"], [""], DegradationFormula(protein_decay_rate, "tetr_p")),
+                 Reaction(["cl_p"], [""], DegradationFormula(protein_decay_rate, "cl_p"))]
+
+    net = Network()
+    net.initialise(species, reactions, regulations)
+
+    end_time = 100
+    s = SimulationSettings("Results", "Time", "Concentration", 0, end_time,
+                           [("LacI Protein", "laci_p"),
+                            ("TetR Protein", "tetr_p"),
+                            ("Cl Protein", "cl_p")])
+    OdeSimulator.visualise(OdeSimulator.simulate(net, s), s)

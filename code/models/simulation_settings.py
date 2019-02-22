2bb7ef5e4197a3dcbf70e3fe1c55c985af530f9a
ilkutkutlar <ilkutkutlar@gmail.com>
2019-02-22 20:32:54 +0000

    Simulation settings is typeless

--- code/models/simulation_settings.py
+++ code/models/simulation_settings.py
@@ -4,13 +4,21 @@ import numpy as np
 
 
 class SimulationSettings:
-    # Tuple[species_name, label]
-    def __init__(self, start_time: float, end_time: float, precision: int,
-                 plotted_species: List[str]):
+    """
+    :param float start_time: of simulation
+    :param float end_time: of simulation
+    :param int precision: how many data points in the given time period
+    :param List[str] plotted_species: Which species to plot in the visualisation
+    """
+
+    def __init__(self, start_time, end_time, precision, plotted_species):
         self.plotted_species = plotted_species
         self.start_time = start_time
         self.end_time = end_time
         self.precision = precision
 
+    """
+    Return time space using the simulation settings
+    """
     def generate_time_space(self):
         return np.linspace(self.start_time, self.end_time, self.precision)

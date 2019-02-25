2e6c1634e4da2da888f8f74490d5208e989990ed
ilkutkutlar <ilkutkutlar@gmail.com>
2019-02-25 13:47:10 +0000

    transcription part of add reaction  dialog is now ina searpate folder

--- code/models/formulae.py
+++ code/models/formulae.py
@@ -93,37 +93,71 @@ class TranscriptionFormula(Formula):
         c = 1 + pow(tf / k, n)
         return 1 / c
 
-    def _and_gate(self):
-        if not filter(lambda x: x.reg_type == RegType.REPRESSION,
-                      self.regulators):
-            return True
-        else:
-            return False
+    @staticmethod
+    def _hill_or_gate(one, two, n, state):
+        a = (pow(state[one.from_gene] / one.k, n))
+        b = (pow(state[two.from_gene] / two.k, n))
+        c = (1 + a + b)
 
-    def _or_gate(self):
-        if filter(lambda x: x.reg_type == RegType.ACTIVATION,
-                  self.regulators):
-            return True
-        else:
-            return False
+        if one.reg_type == RegType.ACTIVATION and two.reg_type == RegType.ACTIVATION:
+            return (a + b) / c
+        elif one.reg_type == RegType.ACTIVATION and two.reg_type == RegType.REPRESSION:
+            return (a + 1) / c
+        elif one.reg_type == RegType.REPRESSION and two.reg_type == RegType.ACTIVATION:
+            return (1 + b) / c
+        else:  # Repression, repression
+            return 1 / c
+
+    @staticmethod
+    def _hill_and_gate(one, two, n, state):
+        one_concent = state[one.from_gene]
+        two_concent = state[two.from_gene]
+
+        one_act = TranscriptionFormula._hill_activator(one_concent, n, one.k)
+        one_rep = TranscriptionFormula._hill_repressor(one_concent, n, one.k)
+
+        two_act = TranscriptionFormula._hill_activator(two_concent, n, two.k)
+        two_rep = TranscriptionFormula._hill_repressor(two_concent, n, two.k)
+
+        if one.reg_type == RegType.ACTIVATION and two.reg_type == RegType.ACTIVATION:
+            return one_act * two_act
+        elif one.reg_type == RegType.ACTIVATION and two.reg_type == RegType.REPRESSION:
+            return one_act * two_rep
+        elif one.reg_type == RegType.REPRESSION and two.reg_type == RegType.ACTIVATION:
+            return one_rep * two_act
+        else:  # Repression, repression
+            return one_rep * two_rep
 
     def compute(self, state):
         # https://www.pnas.org/content/pnas/100/21/11980.full.pdf
 
         # Protein regulates mRNA
-        the_regulation = self.regulators[0] if self.regulators else None
 
         if self.regulators:
-            regulator_concent = state[the_regulation.from_gene]
+            if len(self.regulators) == 1:
+                reg = self.regulators[0]
+                reg_concent = state[reg.from_gene]
 
-            if the_regulation.reg_type == RegType.ACTIVATION:
-                h = self._hill_activator(regulator_concent,
-                                         self.hill_coeff, the_regulation.k)
+                if reg.reg_type == RegType.ACTIVATION:
+                    h = self._hill_activator(reg_concent, self.hill_coeff, reg.k)
+                else:
+                    h = self._hill_repressor(reg_concent, self.hill_coeff, reg.k)
+
+                return h * self.rate
+            elif len(self.regulators) == 2:
+                one = self.regulators[0]
+                two = self.regulators[1]
+
+                if self.input_gate == InputGate.AND:
+                    h = self._hill_and_gate(one, two, self.hill_coeff, state)
+                elif self.input_gate == InputGate.OR:
+                    h = self._hill_or_gate(one, two, self.hill_coeff, state)
+                else:
+                    h = 1
+
+                return h * self.rate
             else:
-                h = self._hill_repressor(regulator_concent,
-                                         self.hill_coeff, the_regulation.k)
-
-            return h * self.rate
+                pass
 
         else:
             return self.rate
@@ -132,10 +166,8 @@ class TranscriptionFormula(Formula):
         for m in mutation:
             if m == "rate":
                 self.rate = mutation[m][0]
-            elif m == "hill_coeff":
+            else:  # m == "hill_coeff":
                 self.hill_coeff = mutation[m][0]
-            else:  # m == "kd"
-                self.k = mutation[m][0]
 
 
 class TranslationFormula(Formula):
--- code/ui/add_reaction_dialog.py
+++ code/ui/add_reaction_dialog.py
@@ -1,11 +1,12 @@
 from PyQt5.QtWidgets import QListWidget, QLabel, QGridLayout, QPushButton, \
-    QVBoxLayout, QDialog, QFormLayout, QLineEdit, QWidget, QCheckBox, QRadioButton, QGroupBox
+    QVBoxLayout, QDialog, QFormLayout, QLineEdit, QWidget, QCheckBox, QRadioButton, QGroupBox, QComboBox
 
 from models.formulae import TranscriptionFormula, TranslationFormula, DegradationFormula, CustomFormula
 from models.reaction import Reaction
 from models.reg_type import RegType
 from models.regulation import Regulation
 from ui import common_widgets
+from ui.add_reaction_dialog_transcription import TranscriptionFields
 from ui.gene_controller import GeneController
 
 
@@ -112,94 +113,8 @@ class AddReactionDialog(QDialog):
 
     # region transcription
 
-    def _add_regulation_clicked(self):
-        pass
-
-    def _refresh_regulations_list(self):
-        self.regulations_list.clear()
-        for x in self.regulations:
-            self.regulations_list.addItem(x)
-
-    def _is_regulated_state_changed(self):
-        if self.is_regulated.isChecked():
-            self.hill_label.setDisabled(False)
-            self.hill.setDisabled(False)
-            self.activation_radio.setDisabled(False)
-            self.repression_radio.setDisabled(False)
-            self.regulator.setDisabled(False)
-            self.reg_label.setDisabled(False)
-            self.k_label.setDisabled(False)
-            self.k.setDisabled(False)
-            self.regulations_label.setDisabled(False)
-        else:
-            self.hill_label.setDisabled(True)
-            self.hill.setDisabled(True)
-            self.activation_radio.setDisabled(True)
-            self.repression_radio.setDisabled(True)
-            self.regulator.setDisabled(True)
-            self.reg_label.setDisabled(True)
-            self.k_label.setDisabled(True)
-            self.k.setDisabled(True)
-            self.regulations_label.setDisabled(True)
-
-    def _init_regulation(self):
-        self.is_regulated = QCheckBox("Is Regulated")
-        self.is_regulated.stateChanged.connect(self._is_regulated_state_changed)
-
-        self.hill_label = QLabel("Hill coefficient: ")
-        self.hill = QLineEdit()
-
-        self._init_add_regulation_box()
-
-        self.regulations_label = QLabel("Regulations")
-        self.regulations_list = QListWidget()
-        self.regulations_list.setFixedHeight(100)
-
-    def _init_add_regulation_box(self):
-        self.add_regulation_box = QGroupBox()
-        fields = QFormLayout()
-
-        self.activation_radio = QRadioButton("Activation")
-        self.repression_radio = QRadioButton("Repression")
-
-        self.reg_label = QLabel("Regulating species:")
-        self.regulator = common_widgets.make_species_combo()
-
-        self.k_label = QLabel("K:")
-        self.k = QLineEdit()
-
-        self.add_button = QPushButton("Add regulation")
-        self.add_button.clicked.connect(self._add_regulation_clicked)
-
-        fields.addRow(self.activation_radio, self.repression_radio)
-        fields.addRow(self.reg_label, self.regulator)
-        fields.addRow(self.k_label, self.k)
-        fields.addRow(self.add_button)
-        self.add_regulation_box.setLayout(fields)
-
     def _init_transcription_fields(self):
-        fields = QFormLayout()
-
-        self.reaction_name1 = QLineEdit()
-        self.transcription_rate = QLineEdit()
-        self.transcribed_species = common_widgets.make_species_combo()
-
-        self._init_regulation()
-
-        fields.addRow(QLabel("Reaction name"), self.reaction_name1)
-        fields.addRow(QLabel("Transcription rate"), self.transcription_rate)
-        fields.addRow(QLabel("Transcribed species: "), self.transcribed_species)
-
-        fields.addRow(self.is_regulated)
-        fields.addRow(self.hill_label, self.hill)
-        fields.addRow(self.regulations_label)
-        fields.addRow(self.regulations_list)
-        fields.addRow(self.add_regulation_box)
-
-        self._is_regulated_state_changed()
-        self.activation_radio.setChecked(True)
-        self.transcription_fields = QWidget()
-        self.transcription_fields.setLayout(fields)
+        self.transcription_fields = TranscriptionFields()
         self.properties_layout.addWidget(self.transcription_fields)
 
     # endregion

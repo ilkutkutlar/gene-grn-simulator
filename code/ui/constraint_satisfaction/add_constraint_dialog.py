a921c8e21761b88979f8e3d9975bd7b9199cbe9d
ilkutkutlar <ilkutkutlar@gmail.com>
2019-04-21 19:15:04 +0300

    Constraint diagram is more user friendly now

--- code/ui/constraint_satisfaction/add_constraint_dialog.py
+++ code/ui/constraint_satisfaction/add_constraint_dialog.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QDialog, QLabel, QFormLayout, QLineEdit, QPushButton
 
 import helper
 from constraint_satisfaction.constraint import Constraint
+from ui import common_widgets
 from ui.gene_presenter import GenePresenter
 
 
@@ -17,24 +18,15 @@ class AddConstraintDialog(QDialog):
         self._init_fields()
         self._init_button()
 
+        self.setMinimumWidth(350)
         self.setLayout(self.layout)
 
     def _button_clicked(self):
-        constraint = self.constraint.text().strip()
-        m = re.match("(.*)(<= | >=)(.*)", constraint)
-
-        if m:
-            species = m.group(1).strip()
-            sign = m.group(2).strip()
-            value = float(m.group(3).strip())
-        else:
-            helper.show_error_message("Constraint syntax error.")
-            return
-
-        time = self.time.text().strip()
-        time = time.split("-")
-        t0 = float(time[0].strip())
-        t1 = float(time[1].strip())
+        sign = self.sign_combo.currentText()
+        species = self.lhs.currentText()
+        value = float(self.rhs.text())
+        t0 = float(self.time_lb.text())
+        t1 = float(self.time_ub.text())
 
         if sign == "<=":
             cons = lambda v: v - value
@@ -42,7 +34,8 @@ class AddConstraintDialog(QDialog):
             cons = lambda v: value - v
         else:
             helper.show_error_message("Constraint syntax error: Unrecognised sign")
-            cons = lambda v: v
+            self.close()
+            return
 
         c = Constraint(species, cons, (t0, t1))
         c.pretty_print = species + sign + str(value) + " for time: " + str(t0) + "s - " + str(t1) + "s"
@@ -57,30 +50,30 @@ class AddConstraintDialog(QDialog):
     def _init_fields(self):
         fields = QFormLayout()
 
-        lhs = QLineEdit()
-        sign_combo = QComboBox()
-        sign_combo.addItems([">=", "<="])
-        rhs = QLineEdit()
+        self.lhs = common_widgets.make_species_combo()
+        self.rhs = QLineEdit()
+        self.rhs.setValidator(helper.get_double_validator())
+        self.rhs.setPlaceholderText("Constraint value")
+        self.sign_combo = QComboBox()
+        self.sign_combo.addItems([">=", "<="])
 
         constraint_box = QHBoxLayout()
-        constraint_box.addWidget(lhs)
-        constraint_box.addWidget(sign_combo)
-        constraint_box.addWidget(rhs)
-        fields.addRow("Constraint:", constraint_box)
+        constraint_box.addWidget(self.lhs)
+        constraint_box.addWidget(self.sign_combo)
+        constraint_box.addWidget(self.rhs)
+        fields.addRow("Constraint (e.g. X >= 20)", constraint_box)
 
-        time_lb = QLineEdit()
-        time_ub = QLineEdit()
+        self.time_lb = QLineEdit()
+        self.time_lb.setValidator(helper.get_double_validator())
+        self.time_lb.setPlaceholderText("Start")
+        self.time_ub = QLineEdit()
+        self.time_ub.setValidator(helper.get_double_validator())
+        self.time_ub.setPlaceholderText("End")
 
         time_box = QHBoxLayout()
-        time_box.addWidget(time_lb)
+        time_box.addWidget(self.time_lb)
         time_box.addWidget(QLabel("-"))
-        time_box.addWidget(time_ub)
-        fields.addRow("Time period", time_box)
-
-        self.constraint = QLineEdit()
-        fields.addRow(QLabel("Constraint (e.g. x >= 10)"), self.constraint)
-
-        self.time = QLineEdit()
-        fields.addRow(QLabel("Time period (e.g. 0 - 20)"), self.time)
+        time_box.addWidget(self.time_ub)
+        fields.addRow("Time period (e.g. 0 - 20)", time_box)
 
         self.layout.addLayout(fields)
